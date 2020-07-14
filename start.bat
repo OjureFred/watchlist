a8252932790d6d0ce87617d2e6bb17e34adbf3c5
Fred Ojure <fredojure@hotmail.com>
2020-07-14 21:15:32 +0300

    Added code for welcoming mail

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 set MOVIE_API_KEY=00441fc41e4b8668d92ff955f563b6ae
 set SECRET_KEY=127654
+set MAIL_USERNAME=fredojure@gmail.com
+SET MAIL_PASSWORD =11>Nyasaye99
 
 virtual\Scripts\python manage.py server
