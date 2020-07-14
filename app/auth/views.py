940e443a5623be131b6894a28d6db994fee19f77
Fred Ojure <fredojure@hotmail.com>
2020-07-14 11:05:53 +0300

    Made corrections which were raising error in code

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,7 +21,7 @@ def login():
 
 @auth.route('/register', methods=["GET", "POST"])
 def register():
-    form = RegistrationForm
+    form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
